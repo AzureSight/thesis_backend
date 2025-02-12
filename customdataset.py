5048b61b3b2268d64f30f3715533b31349e3947d
louise <louisepc@gmail.com>
2025-02-13 04:15:12 +0800

    feb 13 progress

---
+++ customdataset.py
@@ -0,0 +1,45 @@
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+from PIL import Image
+
+val_test_transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+class AugmentedImageFolder(Dataset):
+    def __init__(self, dataset):
+        self.dataset = dataset
+        self.transform = val_test_transform 
+
+    def __len__(self):
+        return 2 * len(self.dataset)
+
+    def __getitem__(self, index):
+        original_index = index // 2
+        image, label = self.dataset[original_index]
+
+        if index % 2 == 1:
+            image = transforms.functional.hflip(image)
+
+        image = self.transform(image)
+        return image, label
+
+class Applytransform(Dataset):
+    def __init__(self, subset):
+        self.subset = subset
+        self.transform = val_test_transform
+
+    def __len__(self):
+        return len(self.subset)
+
+    def __getitem__(self, index):
+        image_path, label = self.subset.dataset.samples[self.subset.indices[index]]  # Get correct file path
+
+        image = Image.open(image_path).convert("RGB")  # Load im
+        
+        if self.transform:
+            image = self.transform(image)  # Apply transformations
+        
+        return image, label
